909353f41a3fd0bdb2377c4cd0d61fc8c46b8cfe
marcosfajunior <marcosfajunior@gmail.com>
2025-04-24 20:16:10 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,24 @@
-const CACHE_NAME = 'pwa-pesquisa-v1';
-const urlsToCache = [
-  'index.html',
-  'quantidade.html',
-  'pergunta1.html',
-  'pergunta2.html',
-  'fim.html',
-  'manifest.json',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
-];
-
-// Instala o Service Worker e faz cache dos arquivos
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Ativa o novo Service Worker e limpa caches antigos
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(
-        keyList.map(function(key) {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/quantidade.html',
+        '/pergunta1.html',
+        '/pergunta2.html',
+        '/fim.html',
+        '/manifest.json',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+      ]);
     })
   );
 });
 
-// Intercepta requisições e responde do cache ou da rede
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });

ae8cca15b8df150ddb181b3fd6b7ca780aac0ebf
marcosfajunior <marcosfajunior@gmail.com>
2025-04-24 20:24:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,18 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('app-cache').then((cache) => {
+    caches.open('pwa-cache').then((cache) => {
       return cache.addAll([
         '/',
-        '/index.html',
-        '/quantidade.html',
-        '/pergunta1.html',
-        '/pergunta2.html',
-        '/fim.html',
-        '/manifest.json',
-        'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+        'index.html',
+        'quantidade.html',
+        'pergunta1.html',
+        'pergunta2.html',
+        'pergunta3.html',
+        'fim.html',
+        'app.js',
+        'manifest.json',
+        'icon.png',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
       ]);
     })
   );
